5534611a18e0c4a024e3d4999890bbdf9c91f740
gilliantuerah <gilliantuerah@gmail.com>
2022-03-16 12:47:15 +0700

    open camera and add button

--- app/src/main/java/com/example/visionapp/MainActivity.kt
+++ app/src/main/java/com/example/visionapp/MainActivity.kt
@@ -1,11 +1,114 @@
 package com.example.visionapp
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.camera2.CameraManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.visionapp.databinding.ActivityMainBinding
+import java.lang.Exception
+
+// TODO: delete later
+private const val TAG = "MyActivity"
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var cameraM: CameraManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        cameraM = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
+        if(allPermissionGranted()){
+            startCamera()
+        }else {
+            ActivityCompat.requestPermissions(
+                this,
+                Constants.REQUIRED_PERMISSIONS,
+                Constants.REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        // TODO: on click button mode1
+        binding.btnMode1.setOnClickListener{
+            Log.i(TAG, "helo aku mode 1")
+        }
+        // TODO: on click button mode2
+        binding.btnMode2.setOnClickListener{
+            Log.i(TAG, "helo aku mode 2")
+        }
+        // TODO: on click button flashlight
+        binding.imgBtnFlash.setOnClickListener{
+            Log.i(TAG, "helo flash")
+        }
     }
+
+    private fun startCamera(){
+        val cameraProviderFuture = ProcessCameraProvider
+            .getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .build()
+                .also { mPreview ->
+                    mPreview.setSurfaceProvider(
+                        binding.viewFinder.surfaceProvider
+                    )
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this,
+                    cameraSelector,
+                    preview
+                )
+
+            }catch (e: Exception){
+                Log.d(Constants.TAG, "startCamera Fail:", e)
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == Constants.REQUEST_CODE_PERMISSIONS){
+            if(allPermissionGranted()){
+                startCamera()
+            }else{
+                Toast.makeText(
+                    this,
+                    "permission not granted by the user",
+                    Toast.LENGTH_SHORT).show()
+
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionGranted() =
+        Constants.REQUIRED_PERMISSIONS.all {
+            ContextCompat.checkSelfPermission(
+                baseContext, it
+            ) == PackageManager.PERMISSION_GRANTED
+        }
 }
